c8624b2e094f46775f90623effa76477cc8fdc82
NMumiatov <NMumiatov@synergy.ru>
2022-04-09 16:08:31 +0300

    pages layout

--- src/views/CardPage.vue
+++ src/views/CardPage.vue
@@ -8,16 +8,16 @@
             <img src="../assets/img/image.jpg" alt="photo" />
           </div>
           <div class="person__info">
-            <div class="person__info-name"><span>ФИО: </span> Jasper Brown</div>
-            <div class="person__info-date">
+            <div class="person__info-item person__info-name"><span>ФИО: </span> Jasper Brown</div>
+            <div class="person__info-item person__info-date">
               <span>Дата рождения: </span> 11.11.1911
             </div>
-            <div class="person__info-phone">
+            <a href="tel:+9 (724)-983-6496" class="person__info-item person__info-phone">
               <span>Телефон: </span> +9 (724)-983-6496
-            </div>
-            <div class="person__info-email">
+            </a>
+            <a href="mailto:jasper.brown@example.com" class="person__info-item person__info-email">
               <span>e-mail: </span> jasper.brown@example.com
-            </div>
+            </a>
           </div>
         </div>
       </div>
